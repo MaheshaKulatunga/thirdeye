096b47b9b2cfa03412b1571ccd8165db6cd8c0ef
Mahesha Kulatunga <mahesha.kulatunga@gmail.com>
2019-07-30 18:16:02 +0100

    Evaluate basic ROC needs personalising

--- evaluate.py
+++ evaluate.py
@@ -2,6 +2,12 @@ import matplotlib.pyplot as plt
 import pickle
 import constants
 import time
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import auc
+from scipy import interp
+import numpy as np
+from itertools import cycle
 
 class Evaluator:
     """ Initialize class """
@@ -18,30 +24,119 @@ class Evaluator:
         plt.legend(['Training', 'Validation'], loc='upper left')
         plt.show()
 
+    """ Plot Confusion matrix """
+    def plot_cm(self, y_true, y_pred):
+        labels = ['Deepfake', 'Real']
+        cm = confusion_matrix(y_true, y_pred, labels)
+        print(cm)
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        cax = ax.matshow(cm)
+        plt.title('Confusion matrix')
+        fig.colorbar(cax)
+        ax.set_xticklabels([''] + labels)
+        ax.set_yticklabels([''] + labels)
+        plt.xlabel('Predicted')
+        plt.ylabel('True')
+        plt.show()
+
+    """ Plot ROC Curve """
+    def plot_roc(self, y_true, y_score):
+        lw = 2
+        n_classes = 2
+        fpr = dict()
+        tpr = dict()
+        roc_auc = dict()
+        for i in range(n_classes):
+            fpr[i], tpr[i], _ = roc_curve(y_true[:, i], y_score[:, i])
+            roc_auc[i] = auc(fpr[i], tpr[i])
+
+        # Compute micro-average ROC curve and ROC area
+        fpr["micro"], tpr["micro"], _ = roc_curve(y_true.ravel(), y_score.ravel())
+        roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+        # Compute macro-average ROC curve and ROC area
+
+        # First aggregate all false positive rates
+        all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+
+        # Then interpolate all ROC curves at this points
+        mean_tpr = np.zeros_like(all_fpr)
+        for i in range(n_classes):
+            mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+
+        # Finally average it and compute AUC
+        mean_tpr /= n_classes
+
+        fpr["macro"] = all_fpr
+        tpr["macro"] = mean_tpr
+        roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+        # Plot all ROC curves
+        plt.figure()
+        plt.plot(fpr["micro"], tpr["micro"],
+                 label='micro-average ROC curve (area = {0:0.2f})'
+                       ''.format(roc_auc["micro"]),
+                 color='deeppink', linestyle=':', linewidth=4)
+
+        plt.plot(fpr["macro"], tpr["macro"],
+                 label='macro-average ROC curve (area = {0:0.2f})'
+                       ''.format(roc_auc["macro"]),
+                 color='navy', linestyle=':', linewidth=4)
+
+        colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
+        for i, color in zip(range(n_classes), colors):
+            plt.plot(fpr[i], tpr[i], color=color, lw=lw,
+                     label='ROC curve of class {0} (area = {1:0.2f})'
+                     ''.format(i, roc_auc[i]))
+
+        plt.plot([0, 1], [0, 1], 'k--', lw=lw)
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Some extension of Receiver operating characteristic to multi-class')
+        plt.legend(loc="lower right")
+        plt.show()
+
     """ Predict test data """
     def predict_test_data(self, x, y, name):
         # TODO Check size is correct and resize?
+        y_true = []
+        y_true_multi = []
+        y_score = []
+        y_pred = []
+
         start = time.time()
         predictions = self.model.predict(x)
         end = time.time()
-        print('{} completed the predictions in {}s'.format(name, (end - start)))
+        print('{} completed the predictions in {}s'.format(name, round((end - start),2)))
         count = 0
         for pred in range(len(predictions)):
             if predictions[pred][0] > predictions[pred][1]:
-                label = 0
+                label = 'Real'
             else:
-                label = 1
+                label = 'Deepfake'
+
+            y_score.append([predictions[pred][0],predictions[pred][1]])
 
             if y[pred][0] > y[pred][1]:
-                true = 0
+                true = 'Real'
+                y_true_multi.append([1,0])
             else:
-                true = 1
+                true = 'Deepfake'
+                y_true_multi.append([0,1])
+
+            y_true.append(true)
+            y_pred.append(label)
 
             if label is true:
                 count += 1
 
         accuracy = count/len(predictions)
-        print(accuracy)
+        self.plot_cm(y_true, y_pred)
+        self.plot_roc(np.array(y_true_multi), np.array(y_score))
+        print('Accuracy: {}%'.format(round((accuracy*100), 2)))
 
     """ Set model """
     def set_model(self, model):
