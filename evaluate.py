e6475b981fcf87fc0f1c1d089eb04912a7e5a353
Mahesha Kulatunga <mahesha.kulatunga@gmail.com>
2019-07-30 19:21:59 +0100

    Evaluate personalised

--- evaluate.py
+++ evaluate.py
@@ -32,8 +32,8 @@ class Evaluator:
         fig = plt.figure()
         ax = fig.add_subplot(111)
         cax = ax.matshow(cm)
-        plt.title('Confusion matrix')
         fig.colorbar(cax)
+        plt.title('Confusion matrix')
         ax.set_xticklabels([''] + labels)
         ax.set_yticklabels([''] + labels)
         plt.xlabel('Predicted')
@@ -51,13 +51,7 @@ class Evaluator:
             fpr[i], tpr[i], _ = roc_curve(y_true[:, i], y_score[:, i])
             roc_auc[i] = auc(fpr[i], tpr[i])
 
-        # Compute micro-average ROC curve and ROC area
-        fpr["micro"], tpr["micro"], _ = roc_curve(y_true.ravel(), y_score.ravel())
-        roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
-
-        # Compute macro-average ROC curve and ROC area
-
-        # First aggregate all false positive rates
+        # Combine false positive rates
         all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
 
         # Then interpolate all ROC curves at this points
@@ -65,26 +59,15 @@ class Evaluator:
         for i in range(n_classes):
             mean_tpr += interp(all_fpr, fpr[i], tpr[i])
 
-        # Finally average it and compute AUC
+        # Get average and calculate AUC
         mean_tpr /= n_classes
-
         fpr["macro"] = all_fpr
         tpr["macro"] = mean_tpr
         roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
 
         # Plot all ROC curves
         plt.figure()
-        plt.plot(fpr["micro"], tpr["micro"],
-                 label='micro-average ROC curve (area = {0:0.2f})'
-                       ''.format(roc_auc["micro"]),
-                 color='deeppink', linestyle=':', linewidth=4)
-
-        plt.plot(fpr["macro"], tpr["macro"],
-                 label='macro-average ROC curve (area = {0:0.2f})'
-                       ''.format(roc_auc["macro"]),
-                 color='navy', linestyle=':', linewidth=4)
-
-        colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
+        colors = cycle(['red', 'blue'])
         for i, color in zip(range(n_classes), colors):
             plt.plot(fpr[i], tpr[i], color=color, lw=lw,
                      label='ROC curve of class {0} (area = {1:0.2f})'
@@ -93,9 +76,9 @@ class Evaluator:
         plt.plot([0, 1], [0, 1], 'k--', lw=lw)
         plt.xlim([0.0, 1.0])
         plt.ylim([0.0, 1.05])
+        plt.title('ROC Curve')
         plt.xlabel('False Positive Rate')
         plt.ylabel('True Positive Rate')
-        plt.title('Some extension of Receiver operating characteristic to multi-class')
         plt.legend(loc="lower right")
         plt.show()
 
