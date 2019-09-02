95febd60489ee0eff23ecd105c3ab5842b574d07
Mahesha Kulatunga <mahesha.kulatunga@gmail.com>
2019-09-02 15:55:51 +0100

    Cleaning for submission

--- example.py
+++ example.py
@@ -1,19 +1,16 @@
 import thirdeye
-
+"""
+An example of the Thirdeye system used to classify unknown videos. 
+"""
 if __name__ == "__main__":
-    ti = thirdeye.Thirdeye(force_t=True, max_for_class=14600, network='odin_v2')
-    # ti.set_network('providence_v2')
-    # ti.set_network('odin_v1')
-    # ti.set_network('odin_v2')
-    # ti.set_network('horus')
-
+    ti = thirdeye.Thirdeye(max_for_class=14600, network='odin_v2')
+    ti.classify()
+    ti.set_network('providence_v2')
+    ti.classify()
+    ti.set_network('odin_v1')
+    ti.classify()
+    ti.set_network('odin_v2')
+    ti.classify()
+    ti.set_network('horus')
+    ti.classify()
     # ti.evaluate()
-    # ti.classify()
-    #
-    # ti.set_network('horus')
-    # ti.train()
-    # ti.perform_preprocessing()
-
-
-    # thirdeye2 = thirdeye.Thirdeye(pre_p=False, force_t=False, name='odin_v1', evaluate=True)
-    # thirdeye3 = thirdeye.Thirdeye(pre_p=False, force_t=False, name='horus', evaluate=True)
